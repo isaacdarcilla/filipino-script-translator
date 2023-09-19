bd9d48e3a417fdf2a01501e2b1532ec8f7df8db6
Isaac A. <isaacdarcilla@gmail.com>
2023-09-19 15:40:37 +0800

    tests: added unit test

--- test/index.spec.ts
+++ test/index.spec.ts
@@ -1,13 +1,49 @@
-import { myPackage } from '../src';
+import { Script, translate } from '../src';
 
-describe('index', () => {
-  describe('myPackage', () => {
-    it('should return a string containing the message', () => {
-      const message = 'Hello';
+describe('baybayin', () => {
+  describe('translate', () => {
+    it('should return a string containing the baybayin text', () => {
+      const text = 'a';
 
-      const result = myPackage(message);
+      const result = translate(text);
 
-      expect(result).toMatch(message);
+      expect(result).toMatch('\u1700');
+    });
+  });
+});
+
+describe('hanunoo', () => {
+  describe('translate', () => {
+    it('should return a string containing the hanunoo text', () => {
+      const text = 'a';
+
+      const result = translate(text, Script.HANUNOO);
+
+      expect(result).toMatch('\u1720');
+    });
+  });
+});
+
+describe('buhid', () => {
+  describe('translate', () => {
+    it('should return a string containing the buhid text', () => {
+      const text = 'nga';
+
+      const result = translate(text, Script.BUHID);
+
+      expect(result).toMatch('\u1745');
+    });
+  });
+});
+
+describe('tagbanwa', () => {
+  describe('translate', () => {
+    it('should return a string containing the tagbanwa text', () => {
+      const text = 'nga';
+
+      const result = translate(text, Script.TAGBANWA);
+
+      expect(result).toMatch('\u1765');
     });
   });
 });

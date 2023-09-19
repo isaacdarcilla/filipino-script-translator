3ff4b2f632ab4766424cb78fbeb6ae4036dfe170
Isaac A. <isaacdarcilla@gmail.com>
2023-09-19 15:38:18 +0800

    chore: added baybayin translation

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,41 @@
-export const myPackage = (taco = ''): string => `${taco} from my package`;
+import toBaybayin from './scripts/baybayin';
+import toBuhid from './scripts/buhid';
+import toHanunoo from './scripts/hanunoo';
+import toTagbanwa from './scripts/tagbanwa';
+
+export enum Script {
+  BAYBAYIN = 'baybayin',
+  HANUNOO = 'hanunoo',
+  BUHID = 'buhid',
+  TAGBANWA = 'tagbanwa',
+}
+
+type ScriptFunction = (text: string) => string;
+
+const scriptConverters: Record<Script, ScriptFunction> = {
+  [Script.BAYBAYIN]: toBaybayin,
+  [Script.HANUNOO]: toHanunoo,
+  [Script.BUHID]: toBuhid,
+  [Script.TAGBANWA]: toTagbanwa,
+};
+
+/**
+ * Translate a given text into a desired script.
+ *
+ * @param text string
+ * @param script Script
+ * @returns string
+ */
+export const translate = (
+  text: string,
+  script: Script = Script.BAYBAYIN
+): string => {
+  const scriptConverter = scriptConverters[script];
+
+  if (scriptConverter) {
+    return scriptConverter(text);
+  }
+
+  // Default to Baybayin if the script is not recognized
+  return toBaybayin(text);
+};
